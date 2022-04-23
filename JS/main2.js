47fbb8340aa251bbddc1fc20f49bd7de9ed72d15
yasindu1997 <yasindusathsara@gmail.com>
2022-04-23 15:08:12 +0530

    lecture done

--- JS/main2.js
+++ JS/main2.js
@@ -134,5 +134,124 @@ else {
 
 console.log("end");
 
+//**************** */
+
+var username = "yasindu@gmail.com";
+var password = "yasindu@123";
+
+// if(){
+
+// }else if(){
+
+// }else if(){
+
+// }else{
+
+// }
+
+var j = "february"
+
+switch (j) {
+    case "january":
+        console.log("Output is 1");
+        break;
+    case "february":
+        console.log("Output is 2");
+        break;
+    case "march":
+        console.log("Outpur is 3");
+        break;
+    default:
+        console.log("default");
+}
+
+
+function alertMe() {
+    alert("Hello Yasindu !");
+}
+
+alertMe();
+
+// *************** loops
+
+
+console.log("Hello Yasindu");
+console.log("Hello Yasindu");
+console.log("Hello Yasindu");
+console.log("Hello Yasindu");
+console.log("Hello Yasindu");
+console.log("Hello Yasindu");
+console.log("Hello Yasindu");
+
+
+for (var i = 100; i > 0; i--) {
+    console.log("Hello Yasindu in loop" + i);
+}
+
+var r = 10;
+
+while (r > 5) {
+    console.log("While Loop");
+    r--;
+}
+
+var t = 10;
+
+do {
+    console.log("do while loop");
+    t--;
+} while (t > 5);
+
+
+// ************** arrays
+
+var e = "yasindu";
+var ee = "sathsara";
+var eee = "Kasun";
+var rrr = "Nipun";
+
+const studnts = ["yasindu", "Sathsara", "Kasun", "Nipun", "Sahan"]; //array
+
+console.log(studnts);
+
+console.log(studnts[4]);
+
+const marks = [25.36, 78.25, 80.00, 90.4];
+var k = marks[0] + 30;
+
+console.log(k);
+
+for (var p = 0; p < studnts.length; p++) {
+    console.log(studnts[p]);
+}
+
+for (var f of studnts) { //in
+    console.log(f);
+}
+
+var u = [[4, 7], [5, 7], [8, 9]];
+
+console.log(u[0][1]);
+
+// *************** OOP - Object Oriented Programming
+
+const shehan = {
+    "name": "shehan",
+    "adress": "colombo",
+    "salary": 250000,
+    "tel.no": "0342222222"
+}
+
+const dell_inspiron_3576 = {
+    "brand": "dell",
+    "processor_model": "intel i3",
+    "ram": 8,
+    "hdd": 1000,
+    "color": "black"
+}
+
+// JSON
+
+
 
 
