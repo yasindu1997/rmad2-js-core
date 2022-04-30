013a8c5e1a5ab8d02c5476d37b784b50cf96b199
yasindu1997 <yasindusathsara@gmail.com>
2022-04-30 12:11:41 +0530

    spa js done

--- JS/main3.js
+++ JS/main3.js
@@ -1,3 +1,31 @@
-document.getElementById("pricing").style.display="none";
-document.getElementById("features").style.display="none";
-document.getElementById("about").style.display="none";
+document.getElementById("pricing").style.display = "none";
+document.getElementById("features").style.display = "none";
+document.getElementById("about").style.display = "none";
+
+function home() {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("pricing").style.display = "none";
+    document.getElementById("features").style.display = "none";
+    document.getElementById("about").style.display = "none";
+}
+
+function pricing() {
+    document.getElementById("pricing").style.display = "block";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("features").style.display = "none";
+    document.getElementById("about").style.display = "none";
+}
+
+function features() {
+    document.getElementById("features").style.display = "block";
+    document.getElementById("pricing").style.display = "none";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("about").style.display = "none";
+}
+
+function about() {
+    document.getElementById("about").style.display = "block";
+    document.getElementById("pricing").style.display = "none";
+    document.getElementById("features").style.display = "none";
+    document.getElementById("home").style.display = "none";
+}
